b14b45c4487636ee1ee400fe14a878088f015d54
Maximiliaan van Dijk <max@maxilibrium.com>
2020-12-09 16:49:20 -0600

    .

--- contracts/LPStaker.sol
+++ contracts/LPStaker.sol
@@ -1,1 +1,25 @@
 pragma solidity ^0.7;
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+contract LPStaker {
+    IUniswapV2Pair pair;
+
+    constructor () {
+        pair = IUniswapV2Pair(0xA8d852A4D5F79860816D7503E6457Ac47964809D);
+    }
+
+    function deposit () public {
+        uint112 reserve0;
+       ( reserve0,, ) = pair.getReserves();
+    }
+
+    function getShare () public view {
+        uint112 totalTokens;
+        (totalTokens,,) = pair.getReserves();
+
+        uint256 balance = pair.balanceOf(msg.sender);
+        uint256 share = balance / totalSupply;
+    }
+
+}
